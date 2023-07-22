e858fd4bca871c6315bab3afe0411105847604be
NehaDasar <fadewar.neha@gmail.com>
2023-07-22 14:02:38 +0530

    module

---
+++ Hotel_Management_Project/src/app/home/home.component.ts
@@ -0,0 +1,33 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { CommonServiceService } from '../common/common-service.service';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
+})
+export class HomeComponent {
+
+  constructor(private router : Router,
+    private commonService : CommonServiceService){}
+
+
+  journey(journey:string){
+
+    if(journey ==='admin'){
+      this.commonService.journey ='admin'
+      this.router.navigateByUrl('admin');
+    }
+    else if(journey === 'owner'){
+      this.commonService.journey='owner'
+      this.router.navigateByUrl('owner');
+    }
+    else{
+      this.commonService.journey = 'user'
+      this.router.navigateByUrl('user');
+   
+  }  
+
+}
+}
